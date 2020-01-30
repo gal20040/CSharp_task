95eb2cb930942c91c726915e088f5a35e9601324
Artem Grodeckiy <gal20040@gmail.com>
2020-01-30 22:58:31 +0300

    Добавил список токенов-операций с весами.

---
+++ ConsoleCalculator/ConsoleCalculator/Tokens.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleCalculator
+{
+    public enum Tokens
+    {
+        Plus = 1,
+        Minus = 1,
+        MultiplicationSign = 2,
+        DivisionSign = 2,
+        OpeningBracket,
+        ClosingOpeningBracket
+    }
+}
