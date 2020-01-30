7e1d819fd89a3aa6ecee325a2014775d5014c79f
Artem Grodeckiy <gal20040@gmail.com>
2020-01-30 22:59:58 +0300

    Метод для обработки входной строки - оставить только цифры и символы операции - вернуть их в виде коллекции.

---
+++ ConsoleCalculator/ConsoleCalculator/InputProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleCalculator
+{
+    public class InputProcessor
+    {
+        public List<char> GetOperandsAndTokens(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return null;
+            }
+
+            var tokens = new List<char>();
+            //var unexpectedSymbols = new List<char>();
+
+            foreach (var symbol in input)
+            {
+                if ((symbol >= '0' && symbol <= '9')
+                    || symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/'
+                    || symbol == '(' || symbol == ')')
+                {
+                    tokens.Add(symbol);
+                }
+                //else if (symbol == ' ')
+                //{
+                //    continue;
+                //}
+                //else
+                //{
+                //    unexpectedSymbols.Add(symbol);
+                //}
+            }
+
+            //if (unexpectedSymbols.Count > 0)
+            //{
+            //    throw new Exception($"Incorrect symbols: {unexpectedSymbols.ToString()} \nin the input: {input}");
+            //}
+
+            return tokens;
+        }
+    }
+}
