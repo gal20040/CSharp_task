7904e321bee76b96d9fc7dafefb72abefc0bd108
Artem Grodeckiy <gal20040@gmail.com>
2020-01-30 19:56:14 +0300

    Обработка входных параметров.

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace ConsoleCalculator
+﻿using System;
+
+namespace ConsoleCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string input = "";
+            if (args.Length > 0)
+                foreach (var arg in args)
+                {
+                    input += arg;
+                }
+            else
+                input = "3+1";
+
+            Console.WriteLine(input);
+            Console.WriteLine(args.Length);
+
+            //Console.ReadLine();
         }
     }
 }
