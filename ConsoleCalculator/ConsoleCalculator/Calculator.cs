d62b3e21e8e92a2921df8328794e336ea0ba4397
Artem Grodeckiy <gal20040@gmail.com>
2020-01-30 19:56:59 +0300

    Простейшие операции, пока для целых чисел.

---
+++ ConsoleCalculator/ConsoleCalculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleCalculator
+{
+    class Calculator
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x - y;
+        }
+
+        public float Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
