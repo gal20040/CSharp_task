d86ab1e555c77ce10565f5fa9d43b8a769937965
Artem Grodeckiy <gal20040@gmail.com>
2020-02-02 21:49:10 +0300

    ListsComparer для сравнения.

---
+++ ArtList/ArtList.Tests/Helpers/ListsComparer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace ArtList.Tests.Helpers
+{
+    public class ListsComparer
+    {
+        public bool IsEqual(List<int> list, ArtList<int> artList, out string comparisonResult)
+        {
+            if (list.Capacity != artList.Capacity)
+            {
+                comparisonResult = $"\nCapacities are not equal:" +
+                                   $"\n\t{nameof(list)}.Capacity={list.Capacity}" +
+                                   $"\n\t{nameof(artList)}.Capacity={artList.Capacity}";
+                return false;
+            }
+
+            if (list.Count != artList.Count)
+            {
+                comparisonResult = $"\nCounts are not equal:" +
+                                   $"\n\t{nameof(list)}.Count={list.Count}" +
+                                   $"\n\t{nameof(artList)}.Count={artList.Count}";
+                return false;
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] != artList[i])
+                {
+                    comparisonResult = $"\nItems values are not equal at {i} index:" +
+                                    $"\n\t{nameof(list)}[{i}]={list[i]}" +
+                                    $"\n\t{nameof(artList)}[{i}]={artList[i]}";
+                    return false;
+                }
+            }
+
+            comparisonResult = string.Empty;
+            return true;
+        }
+    }
+}
