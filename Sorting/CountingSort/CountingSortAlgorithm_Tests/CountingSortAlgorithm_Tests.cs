976d9ee44872540ea0247b6b4ff03a65c0acef75
Artem Grodeckiy <gal20040@gmail.com>
2020-03-09 23:52:12 +0300

    Доработал и добавил тесты.

--- Sorting/CountingSort/CountingSortAlgorithm_Tests/CountingSortAlgorithm_Tests.cs
+++ Sorting/CountingSort/CountingSortAlgorithm_Tests/CountingSortAlgorithm_Tests.cs
@@ -1,30 +1,98 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace CountingSortAlgorithm_Tests
 {
     [TestClass]
     public class CountingSortAlgorithm_Tests
     {
+        #region PrepareNumbers
+        [TestMethod]
+        public void PrepareNumbers()
+        {
+            const int initialCount = 0;
+
+            var expected = new Dictionary<int, int>
+            {
+                { -2, initialCount },
+                { -1, initialCount },
+                {  0, initialCount },
+                {  1, initialCount },
+                {  2, initialCount },
+                {  3, initialCount }
+            };
+
+            var inputArray = new int[] { -2, -1, 0, 1, 2, 3 };
+            var actual = new CountingSortAlgorithm().PrepareNumbers(inputArray);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PrepareNumbers_repeatedNumber_exception()
+        {
+            var inputArray = new int[] { -2, -2, 0, 1, 2, 3 };
+            new CountingSortAlgorithm().PrepareNumbers(inputArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PrepareNumbers_lesserNumber_exception()
+        {
+            var inputArray = new int[] { -2, -3, 0, 1, 2, 3 };
+            new CountingSortAlgorithm().PrepareNumbers(inputArray);
+        }
+
+        [TestMethod]
+        public void PrepareNumbers_emptyArray()
+        {
+            var expected = new Dictionary<int, int>();
+
+            var inputArray = new int[] { };
+            var actual = new CountingSortAlgorithm().PrepareNumbers(inputArray);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region Sort
         [TestMethod]
         public void Sort_emptyArray()
         {
             var expected = new int[] { };
 
+            var possibleValues = new int[] { };
             var inputArray = new int[] { };
-            var actual = new CountingSortAlgorithm().Sort(inputArray);
+            var actual = new CountingSortAlgorithm().Sort(possibleValues, inputArray);
 
             CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Sort()
+        public void Sort_10123102_ok()
         {
-            var expected = new int[] { 0, 1, 1, 2, 2, 2, 3, 3, 3 };
+            var expected = new int[] { 0, 0, 1, 1, 1, 2, 2, 3 };
 
-            var inputArray = new int[] { };
-            var actual = new CountingSortAlgorithm().Sort(inputArray);
+            var possibleValues = new int[] { 0, 1, 2, 3 };
+            var inputArray = new int[] { 1, 0, 1, 2, 3, 1, 0, 2 };
+            var actual = new CountingSortAlgorithm().Sort(possibleValues, inputArray);
 
             CollectionAssert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Sort_101231minus2minus39_ok()
+        {
+            var expected = new int[] { -3, -2, 0, 1, 1, 1, 2, 3, 9 };
+
+            var possibleValues = new int[] { -3, -2, 0, 1, 2, 3, 9 };
+            var inputArray = new int[] { 1, 0, 1, 2, 3, 1, -2, -3, 9 };
+            var actual = new CountingSortAlgorithm().Sort(possibleValues, inputArray);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        #endregion
     }
 }
