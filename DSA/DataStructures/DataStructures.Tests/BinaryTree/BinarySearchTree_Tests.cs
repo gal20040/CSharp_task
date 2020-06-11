0d8a364839aa8a0df8a9cb3859b1736829669690
Artem Grodetskiy <gal20040@gmail.com>
2020-06-11 23:58:36 +0300

    Добавил начало BinarySearchTree.

---
+++ DSA/DataStructures/DataStructures.Tests/BinaryTree/BinarySearchTree_Tests.cs
@@ -0,0 +1,27 @@
+﻿using DataStructures.BinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DataStructures.Tests.BinaryTree
+{
+    [TestClass]
+    public class BinarySearchTree_Tests : BaseTest
+    {
+        private readonly Random random = new Random();
+
+        #region Insert
+        [TestMethod]
+        public void Insert_()
+        {
+            var tree = new BinarySearchTree<int>();
+
+            tree.Insert(50);
+            //tree.Insert(40);
+            //tree.Insert(70);
+            //tree.Insert(45);
+
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ DSA/DataStructures/DataStructures/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,59 @@
+﻿namespace DataStructures.BinaryTree
+{
+    public class BinarySearchTree<T> : IBinarySearchTree<T>
+    {
+        private TreeNode<T> rootNode = null;
+        private TreeNode<T> currentNode = null;
+
+        private int nodeCount;
+
+        public BinarySearchTree() { }
+        
+        public BinarySearchTree(BinarySearchTree<T> tree)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TreeNode<T> Delete(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Find(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TreeNode<T> FindNode(T item, TreeNode<T> parent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TreeNode<T> GetRoot() => rootNode;
+
+        public TreeNode<T> Insert(T item)
+        {
+            if (rootNode == null)
+            {
+                rootNode = new TreeNode<T>(item);
+                //currentNode = rootNode;
+
+                return rootNode;
+            }
+
+            if (currentNode == null) currentNode = rootNode;
+
+            //if (item < currentNode.Data)
+            //{
+            //    if (currentNode.getLeftChild())
+            //}
+
+            return currentNode;
+        }
+
+        public TreeNode<T> Update(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
