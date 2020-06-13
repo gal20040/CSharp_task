f315a3a105d17eb1745a0ba0da6564d267619763
Artem Grodetskiy <gal20040@gmail.com>
2020-06-14 00:11:24 +0300

    Добавил ещё тестов для Insert для дерева.

--- DSA/DataStructures/DataStructures.Tests/BinaryTree/BinarySearchTree_Tests.cs
+++ DSA/DataStructures/DataStructures.Tests/BinaryTree/BinarySearchTree_Tests.cs
@@ -11,7 +11,7 @@ namespace DataStructures.Tests.BinaryTree
 
         #region Insert
         [TestMethod]
-        public void Insert_()
+        public void Insert_mixedTree_ok()
         {
             const string expected = "40 45 50 70";
 
@@ -21,6 +21,64 @@ namespace DataStructures.Tests.BinaryTree
             actualTree.Insert(40);
             actualTree.Insert(70);
             actualTree.Insert(45);
+
+            var actual = actualTree.GetRoot().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Insert_leftDegeneratedTree_ok()
+        {
+            const string expected = "10 20 30 40";
+
+            var actualTree = new BinarySearchTree();
+
+            actualTree.Insert(40);
+            actualTree.Insert(30);
+            actualTree.Insert(20);
+            actualTree.Insert(10);
+
+            var actual = actualTree.GetRoot().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Insert_rightDegeneratedTree_ok()
+        {
+            const string expected = "60 70 80 90";
+
+            var actualTree = new BinarySearchTree();
+
+            actualTree.Insert(60);
+            actualTree.Insert(70);
+            actualTree.Insert(80);
+            actualTree.Insert(90);
+
+            var actual = actualTree.GetRoot().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Insert_leftAndRightDegeneratedTree_ok()
+        {
+            const string expected = "10 20 30 40 50 60 70 80 90";
+
+            var actualTree = new BinarySearchTree();
+
+            actualTree.Insert(50);
+
+            actualTree.Insert(40);
+            actualTree.Insert(30);
+            actualTree.Insert(20);
+            actualTree.Insert(10);
+
+            actualTree.Insert(60);
+            actualTree.Insert(70);
+            actualTree.Insert(80);
+            actualTree.Insert(90);
 
             var actual = actualTree.GetRoot().ToString();
 
